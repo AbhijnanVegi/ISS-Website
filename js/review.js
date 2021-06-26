2675d6350e74878d4cf0d185899616ee39943d65
Abhijnan Vegi <abhijnan.vegi@gmail.com>
2021-06-27 01:27:28 +0530

    Updated to changes in main

--- js/review.js
+++ js/review.js
@@ -10,5 +10,34 @@ function addReview()
     $("#error").css("display","none")
     $("#reviews").append("<tr><td>"+name+"</td><td>"+skill+"</td><td>"+level+"</td></tr>")
     $("#reviewer")[0].reset()
+
+    var review = {
+        "name": name,
+        "skill": skill,
+        "level" : level
+    }
+
+    var reviewjson = localStorage.getItem("Reviews");
+    var reviews = JSON.parse(reviewjson);
+    if(reviews == null){
+    reviews = [];
+    }
+    reviews.push(review);
+    localStorage.setItem("Reviews", JSON.stringify(reviews));
     return true
 }
+
+function loadReviews()
+{
+    var reviewjson = localStorage.getItem("Reviews");
+    var reviews = JSON.parse(reviewjson);
+    if(reviews == null){
+    reviews = [];
+    }
+
+    reviews.forEach(function(obj) {
+        $("#reviews").append("<tr><td>" + obj.name + "</td><td>" + obj.skill + "</td><td>" + obj.level + "</td></tr>")
+    })
+}
+
+window.onload = loadReviews;
